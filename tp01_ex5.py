9a0e9329f63f866dadd72bc2bf34126ca9ce0a6d
romy-m <71267267+romy-m@users.noreply.github.com>
2021-10-16 23:08:46 +0200

    Update tp01_ex5.py

--- tp01_ex5.py
+++ tp01_ex5.py
@@ -19,6 +19,36 @@ Contrainte : Si la personne possède la carte mensuelle,
              il ne faut pas lui demander d'autres informations."
 """
 ### Déclaration et Initialisation des variables
+prix_base : int = 10
+rabais_e : int = 2
+rabais_m : int = 3
+forfait_f : int = 1
+prix_a_payer : int = 0
+
+# input user
+c_m = input("Possédez-vous la carte mensuelle ? (oui ou non ) ")
+
 
 
 ### Séquence d'opération
+
+if c_m.upper() == "OUI":# test si user utilise une carte memebre
+    prix_a_payer = 0
+    print("Le prix à payer est : {} CHF".format(prix_a_payer))
+else:# test si user n'as pas de carte membre
+    c_membre = input("Possédez-vous la carte membre?  (oui ou non ) ")
+    c_e = input("Possédez-vous la carte étudiante ? (oui ou non ) ")
+    f_f = input("Bénéficiez-vous du forfait famille ? (oui ou non ) ")
+
+    # transformation de saisie
+    membre = c_membre.upper()
+    ce = c_e.upper()
+    ff = f_f.upper()
+
+    # calcul le prix à payer
+    if membre == "OUI" and ce == "OUI" or membre == "OUI" and ff == "OUI" and ce:
+        prix_a_payer = prix_base - rabais_m - rabais_e
+    elif membre == "OUI" and ff == "OUI":
+        prix_a_payer = prix_base - rabais_m - forfait_f
+
+    print("Le prix à payer est {} CHF ".format(prix_a_payer))
